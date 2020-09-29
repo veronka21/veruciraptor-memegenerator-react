3088840288abe39d687e53c9af94c412d2293020
Takács Veronika <roni9764@gmail.com>
2020-09-29 20:34:29 +0200

    Refactor MemeEdit.js skeleton

--- src/components/MemeEdit.js
+++ src/components/MemeEdit.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 function MemeEdit(props) {
   const [memes, setMemes] = useState([])
-  const [meme, setMeme] = useState({});
 
   let path = window.location.pathname;
   let id = path.split("/")[path.split("/").length - 1];
@@ -12,13 +11,7 @@ function MemeEdit(props) {
     axios
     .get("https://api.imgflip.com/get_memes")
     .then((response) => setMemes(response.data.data.memes))
-    .then(() => {
-      let memeToEdit = memes.filter(meme => meme.id === id);
-    setMeme(memeToEdit)
-    });
-  }, [id, memes])
-
-  console.log(meme);
+  }, [])
 
   return memes.filter(meme => meme.id === id)
   .map(meme => 
